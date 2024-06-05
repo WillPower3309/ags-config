0d42255c0034961163a385b1ebefcc96787e67f4
William McKinnon <contact@willmckinnon.com>
2024-06-04 21:04:53 -0400

    added base ags config

---
+++ config.js
@@ -0,0 +1,16 @@
+//import { applauncher } from "./widgets/app-launcher.js";
+import { NotificationPopups } from "./widgets/notification-popups.js"
+import { PowerMenu } from "./widgets/power-menu.js"
+import { QuickSettings } from "./widgets/quick-settings.js"
+import { TopBar } from "./widgets/top-bar.js"
+
+App.config({
+    style: App.configDir + "/style.css",
+    windows: [
+        //applauncher,
+        NotificationPopups(),
+        PowerMenu(),
+        TopBar(),
+        QuickSettings(),
+    ],
+})
